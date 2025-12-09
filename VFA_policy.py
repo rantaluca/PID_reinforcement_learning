81fef38632b6931b6ea058b3b7c466fcebe11111
Romainnnnnnnnn <romvir6760@gmail.com>
2025-12-09 02:36:05 -0500

    Création de politique VFA

--- VFA_policy.py
+++ VFA_policy.py
@@ -1,32 +1,47 @@
-import sys866_lib as lib
+import sys866_lib_Windows as lib
 import numpy as np
 
 class RLSTDPolicy(lib.Policy):
 
+
+
     def __init__(
         self, 
         consigne, 
-        consigne_tresh=0.97, 
-        loss_params={'error':1.0, 'dep':0.5, 'conv':0.3}, 
+        consigne_tresh=0.98, 
+        loss_params={'error':0.5, 'dep':10, 'conv':0.5}, 
         dt=0.1,
-        Kp_ref = 5.0,
-        Ki_ref = 1.0,
+        # Références pour la normalisation des gains
+        Kp_ref = 1.0,
+        Ki_ref = 0.1,
+        # Pas
         dKp = 0.01,
         dKi = 0.001,
         dKp2 = 0.05,
         dKi2 = 0.005,
+        #Limites
+        Kp_bounds=(0.0, 1.0),
+        Ki_bounds=(0.0, 1.0),
+        # Paramètres RLSTD
         beta = 100,
         epsilon = 0.1,
         gamma = 0.99,
-        Integral_ref = 1, # Au plus ce facteur est élevé, au plus l'erreur intégrale normalisée pénalise
+        Integral_ref = 1.0, # Au plus ce facteur est élevé, au plus l'erreur intégrale normalisée pénalise
+        # Gains initiaux (P2)
+        Kp_init = 0.4,
+        Ki_init = 0.04,
     ):
 
         super().__init__(
-            consigne, 
-            consigne_tresh, 
-            loss_params, 
-            dt)
+            consigne = consigne, 
+            consigne_tresh = consigne_tresh, 
+            loss_params = loss_params, 
+            dt = dt)
     
+        self.Kp = Kp_init
+        self.Ki = Ki_init
+        self.Kd = 0.0
+
         self.Kp_ref = Kp_ref
         self.Ki_ref = Ki_ref
 
@@ -34,6 +49,9 @@ class RLSTDPolicy(lib.Policy):
         self.dKi = dKi
         self.dKp2 = dKp2
         self.dKi2 = dKi2
+
+        self.Kp_bounds = Kp_bounds
+        self.Ki_bounds = Ki_bounds
 
         self.beta = beta
         self.epsilon = epsilon
@@ -44,17 +62,22 @@ class RLSTDPolicy(lib.Policy):
         self.Integral_ref = Integral_ref
 
         # Variables RLSTD
-        self.number_of_features = 9
-        self_theta = np.zeros(self.number_of_features)
+        self.number_of_features = 8
+        self.theta = np.zeros(self.number_of_features)
         self.A = np.eye(self.number_of_features) * self.beta
         self.last_phi = None
         self.last_reward = 0.0
 
+        self.actions_set = 1
 
+        #Pour désactiver / activer l'entrainement de la VFA
+        self.train_value_function = True
 
 
     
     def compute_features(self, observation):
+
+        consigne, error, command, output = observation
 
         ## Calcul des features pour la VFA (phi)
 
@@ -62,31 +85,35 @@ class RLSTDPolicy(lib.Policy):
 
         bias = 1.0
 
-        #2 Calcul de l'erreur statique
+        #2 Calcul de l'erreur statique normalisée
 
-        error = self.consigne - observation[0]
-        error_norm = error / self.consigne
+        if consigne == 0:
+            error_norm = 0.0
+        else:
+            error_norm = error / consigne
 
         #3 Calcul de l'erreur carrée
 
-        error_norn_squared = error_norm^2
+        error_norm_squared = error_norm ** 2
 
         #4 Calcul de l'erreur dérivée
 
         derivative_error = error - self.prev_error
-        derivative_error_norm = derivative_error / self.consigne
+        if consigne == 0:
+            derivative_error_norm = 0.0
+        else:
+            derivative_error_norm = derivative_error / consigne
 
         #5 Calcul de l'indicateur de non-convergence
 
-        if (abs(error) >= self.consigne * (1.0 - self.consigne_tresh)):
+        if (abs(error) >= consigne * (1.0 - self.consigne_tresh)):
             conv_norm = 1.0
         else:
             conv_norm = 0.0
         
         #6 Calcul de l'erreur intégrale nomalisée
 
-        self.cumulative_error += error_norm * self.dt
-        integrative_error_norm = lib.sat(self.cumulative_error / self.Integral_ref)
+        integrative_error_norm = max(-1.0, min(1.0, self.cumulative_error / self.Integral_ref))
 
         #78 Gains normalisés
 
@@ -96,7 +123,7 @@ class RLSTDPolicy(lib.Policy):
         phi = np.array([
             bias,
             error_norm,
-            error_norn_squared,
+            error_norm_squared,
             derivative_error_norm,
             conv_norm,
             integrative_error_norm,
@@ -104,26 +131,19 @@ class RLSTDPolicy(lib.Policy):
             Ki_norm,
         ], dtype=float)
 
-        #On garde l'erreur actuelle pour le prochain step
-
         self.prev_error = error
 
         return phi
     
 
 
-
-
     def update_value_function(self, phi_next):
         
         ### Algorithme RLSTD
 
-
         ## Première itération
 
         if self.last_phi is None:
-
-            # Première itération : on stocke juste pour la prochaine
             self.last_phi = phi_next.copy()
             return
         
@@ -151,15 +171,18 @@ class RLSTDPolicy(lib.Policy):
         a = 1.0 + float(u.T @ v)
         k = v / a
 
-        # Mises à jour
+        # Mise à jour theta
         self.theta = theta + k * delta
+
+        #Mise à jour A
         g_col = A @ u
         self.A = A - np.outer(k, g_col)
+
+        #Mise à jour mémoire
         self.last_phi = phi_next.copy()
         
 
 
-    
     def compute_loss(self, observation):
 
         ### Fonction de coût via classe parente
@@ -168,7 +191,8 @@ class RLSTDPolicy(lib.Policy):
 
         self.last_reward = -current_loss
 
-        return current_loss, running_loss, self.last_reward
+        return current_loss, running_loss
+
 
 
     def enumerate_actions(self):
@@ -183,6 +207,8 @@ class RLSTDPolicy(lib.Policy):
                 actions.append((dKp, dKi))
         return actions
     
+
+
     def enumerate_actions_2(self):
     
         ### Génération des 25 actions possibles (+/=/- dKp1 ou +/=/- dkp2, +/=/- dKi1 ou +/=/- dKi2)
@@ -219,15 +245,76 @@ class RLSTDPolicy(lib.Policy):
             (0.0, 0.0),
         ]
 
+        return actions
+
+
+
+    def next_action(self, observation):
+
+        consigne, error, command, output = observation
+
+        if self.actions_set == 0:
+            actions = self.enumerate_actions()
+        else:
+            actions = self.enumerate_actions_2()
+
+        #Politique epsilon-greedy basée sur équation de Bellman
+            
+        # Mise à jour de l'erreur cumulative
+
+        if consigne != 0:
+            self.cumulative_error += (error / consigne) * self.dt
         
+        # Calcul des features du nouveau état
 
+        phi_current = self.compute_features(observation)
 
-    
-    
+        # Mise à jour de la VFA si activée
 
-    
+        if self.train_value_function:
+            self.update_value_function(phi_current)
+        
+        # Calcul de la perte actuelle
 
-    #def next_action(self, observation):
+        c_t = self.current_loss
+        
+        # Epsilon-greedy sur ensemble d'action discrètes
+            # Si epsilon vaut 0, politique normale
+
+        if np.random.rand() < self.epsilon:
+            #Exploration
+            dKp, dKi = actions[np.random.randint(len(actions))]
+        else:
+            #Exploitation
+            best_score = np.inf
+            best_dKp, best_dKi = 0.0, 0.0
+
+            for dKp_cand, dKi_cand in actions:
+
+                Kp_cand = np.clip(self.Kp + dKp_cand, *self.Kp_bounds)
+                Ki_cand = np.clip(self.Ki + dKi_cand, *self.Ki_bounds)
+
+                phi_cand = phi_current.copy()
+
+                phi_cand[6] = Kp_cand / self.Kp_ref
+                phi_cand[7] = Ki_cand / self.Ki_ref
+
+                V_cand = float(self.theta @ phi_cand)
+
+                score = c_t + self.gamma * V_cand
+
+                if score < best_score:
+                    best_score = score
+                    best_dKp, best_dKi = dKp_cand, dKi_cand
+
+            dKp, dKi = best_dKp, best_dKi
+
+        self.Kp = float(np.clip(self.Kp + dKp, *self.Kp_bounds))
+        self.Ki = float(np.clip(self.Ki + dKi, *self.Ki_bounds))
+        self.Kd = 0.0
+
+        return self.Kp, self.Ki, self.Kd
+
 
         # Ici c'est à creuser vu que mon entrainement a 2 phases :
         ## Phase 1 : echauffement des poids avec P2 (donc indépendamment des actions du dessus)
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import sys866_lib_Windows as ps
-#import sys866_lib as ps
+#import sys866_lib_Mac as ps
+from VFA_policy import RLSTDPolicy
 
 
 instance = ps.SimulinkInstance(
-    sim_name='Simulation_SYS866_final',
+    sim_name='Simulation_simulink_P2_Windows',
     pid_block='PID_Controller',
     consigne_block=None,
     init_Kp=0.4,
     init_Ki=0.04,
     init_Kd=0.0,
     dt_sim=0.01,
-    pre_script='Projet_SYS866_final'
+    pre_script='Simulation_matlab_P2'
 )
 
 policy = ps.Policy(
@@ -19,6 +20,31 @@ policy = ps.Policy(
     loss_params={'error':5.0, 'dep':0.5, 'conv':0.3},
     dt=0.1 
 )
+
+# policy = RLSTDPolicy(
+#     consigne=None,
+#     consigne_tresh=0.98,
+#     loss_params={'error':0.5, 'dep':10, 'conv':0.5},
+#     dt=0.1,
+#     Kp_ref=1.0,
+#     Ki_ref=0.1,
+#     dKp=0.01,
+#     dKi=0.001,
+#     dKp2=0.05,
+#     dKi2=0.005,
+#     Kp_bounds=(0.0, 1.0),
+#     Ki_bounds=(0.0, 1.0),
+#     beta=100,
+#     epsilon=0.1,
+#     gamma=0.99,
+#     Integral_ref=1.0,
+#     Kp_init=0.4,
+#     Ki_init=0.04,
+# )
+
+
+
+
 
 episode_loop = ps.EpisodeLoop(
     env=instance,
